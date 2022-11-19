7456feec717ec22ac9df5157507f6fc0fc4faaaa
xiezihen <frank.xie@mail.utoronto.ca>
2022-11-18 20:08:52 -0500

    initial commit

---
+++ Controllers/TeacherController.cs
@@ -0,0 +1,30 @@
+﻿using BlogProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogProject.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult List(string SearchKey = null)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            IEnumerable<Teacher> teachers = controller.ListTeachers(SearchKey);
+            return View(teachers);
+        }
+        public ActionResult Show(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            Teacher teacher = controller.FindTeacher(id);
+            return View(teacher);
+        }
+    }
+}
